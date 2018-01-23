4e1a2a0a0106c51636ab1fdb5cc909ad067735e3
bhargav <bhargav.r.raut@gmail.com>
2018-01-23 17:29:59 +0530

    modified to work with mongoid-versioned-atomic

--- lib/mongoid/elasticsearch/callbacks.rb
+++ lib/mongoid/elasticsearch/callbacks.rb
@@ -7,8 +7,17 @@ module Mongoid
         after_save :update_es_index
         after_destroy :update_es_index
         
+        ## THIS FUNCTION HAS BEEN UPDATED TO DO THE UPDATE ONLY IF THE OP_SUCCESS IS TRUE OR NIL, IN CASE THE RECORD RESPONDS TO OP_SUCCESS.
         def update_es_index
-          es_update
+          if self.respond_to? :op_success
+            if self.op_success.nil?
+              es_update
+            else
+              es_update if self.op_success == true
+            end
+          else
+            es_update
+          end
         end
       end
 
